628a2e0b2d5153aee586a4abd184f97f959d644c
BAE <lexon1209@gmail.com>
2025-01-03 16:34:30 +0900

    Feat: UserTableViewCell Implement

--- SecondWeek/0103/UserTableViewController.swift
+++ SecondWeek/0103/UserTableViewController.swift
@@ -9,19 +9,31 @@ import UIKit
 
 class UserTableViewController: UITableViewController {
 
+    let name = ["고래밥", "칙촉", "카스타드"]
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        name.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "userCell", for: indexPath) as! UserTableViewCell
+        
+        cell.profileImageView.backgroundColor = .cyan
+        cell.nameLabel.text = name[indexPath.row]
+        cell.messageLabel.text = "상메"
+        
+        cell.nameLabel.font = .boldSystemFont(ofSize: 15)
+        cell.messageLabel.font = .systemFont(ofSize: 12)
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        100
     }
 
 }
